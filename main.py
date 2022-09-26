48f1e3281dfc584fd0d813db3177184bfbf3f4b6
jfhaines <joefhaines@gmail.com>
2022-09-26 13:47:32 +1000

    Added .sh files for executing program

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from import_json import pokemon_data
 from bag import ItemBag
 import sys
 
+
 mode = 'normal'
 arg = sys.argv[1]
 if arg == '--hard' or arg == '-h':
@@ -17,6 +18,24 @@ elif arg == '--easy' or arg == '-e':
     mode = 'easy'
 elif arg == '--normal' or arg == '-n':
     pass
+
+print("\n"
+      "\n"
+      "Welcome to the Kanto region. This world is full of pokemon. " \
+      "Here, you can move around the map, collect items, " \
+      "catch pokemon and battle trainers. The '@' symbol on the map " \
+      "represents your player. The '?' symbol represents an item. " \
+      "The '!' symbol represents a trainer. The '*' symbol " \
+      "represents a patch of grass (which sometimes contain pokemon)." \
+      "Each Pokemon has HP representing their health, use a health potion "
+      "if it gets low. Each Pokemon has a set of moves. Each move " \
+      "has different damage. Each time you use a move, the move's PP will "
+      "decrease by 1. If it drops to 0, use a Move Potion to restore PP, " \
+      "so that you can keep using that move. Use a PokeBall on wild Pokemon " \
+      "to try and catch them. The lower the opponent Pokemon's HP, the more " \
+      "likely you are to catch them. Once caught, the Pokemon will be " \
+      "added to your collection, and you can switch to it in battle. "
+      "If you have no pokemon with HP remaining, the game ends. Good luck!")
 
 map = Map(10, 10)
 player = Player()
@@ -38,6 +57,6 @@ else:
         player.items.add(PokeBall())
         player.items.add(MovePotion())
 
-
 map.set([0, 0], player)
-me.move(map)
+
+player.move(map)
--- run-normal.sh
+++ run-normal.sh
@@ -6,4 +6,4 @@ echo "Checking and/or installing necessary files."
 
 pip install -r requirements.txt
 
-python main.py --b
+python main.py --n
